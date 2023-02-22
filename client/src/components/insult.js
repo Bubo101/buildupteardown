4390f1b09b6a95a6307c42811a44dab3820cb4ae
Boden <boden.bradley@gmail.com>
2023-02-22 14:45:40 -0800

    got insult api working

--- client/src/components/insult.js
+++ client/src/components/insult.js
@@ -1,23 +1,22 @@
 import React, { useState } from "react";
 
 function InsultGenerator() {
-  const [insult, setInsult] = useState("");
+const [insult, setInsult] = useState("");
 
-  const fetchInsult = async () => {
+const fetchInsult = async () => {
     const response = await fetch(
-      "https://crossorigin.me/https://evilinsult.com/generate_insult.php?lang=en&type=json"
+        `https://insult.mattbas.org/api/insult.json`
     );
     const data = await response.json();
-    console.log(data)
     setInsult(data.insult);
-  };
+    };
 
-  return (
+return (
     <div>
-      <button onClick={fetchInsult}>Insult me</button>
-      {insult && <p>{insult}</p>}
+        <button onClick={fetchInsult}>Insult me</button>
+        {insult && <p>{insult}</p>}
     </div>
-  );
+);
 }
 
 export default InsultGenerator;
