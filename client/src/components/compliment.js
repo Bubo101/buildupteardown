a3eefdf3713cf942de8cc4f214a367bd2e15bf1b
Boden <boden.bradley@gmail.com>
2023-05-10 15:36:00 -0700

    updated comp

--- client/src/components/compliment.js
+++ client/src/components/compliment.js
@@ -14,7 +14,7 @@ function ComplimentGenerator() {
 
     return (
     <div className="container" >
-        <button className="button" onClick={fetchCompliment}>Compliment Me Now Please!</button>
+        <button className="button" onClick={fetchCompliment}>Compliment Me Now, I deserve it!</button>
         <div className="box">
             <p className="compliment">{compliment}</p>
         </div>
